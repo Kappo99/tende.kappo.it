0fd079c7a45237edc1c1af38b740b0e4e661ca64
Kappons <simonecappone1607@gmail.com>
2026-01-19 21:35:06 +0100

    input

--- dashboard/src/app/page.tsx
+++ dashboard/src/app/page.tsx
@@ -1,17 +1,21 @@
-import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input"
 import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
 
 export default function Home() {
   return (
     <main className="container mx-auto px-4 py-8">
       <Tabs defaultValue="account" className="w-100">
-        <TabsList>
-          <TabsTrigger value="account">SENSORE VENTO</TabsTrigger>
-          <TabsTrigger value="password">SENSORE PIOGGIA</TabsTrigger>
-          <TabsTrigger value="password">REGISTRO ALLARMI</TabsTrigger>
-        </TabsList>
-        <TabsContent value="account">Make changes to your account here.</TabsContent>
-        <TabsContent value="password">Change your password here.</TabsContent>
+        <div className="flex justify-between w-full">
+          <TabsList>
+            <TabsTrigger value="vento">SENSORE VENTO</TabsTrigger>
+            <TabsTrigger value="pioggia">SENSORE PIOGGIA</TabsTrigger>
+            <TabsTrigger value="allarmi">REGISTRO ALLARMI</TabsTrigger>
+          </TabsList>
+          <Input/>
+        </div>
+        <TabsContent value="vento">Make changes to your account here.</TabsContent>
+        <TabsContent value="pioggia">Change your password here.</TabsContent>
+        <TabsContent value="allarmi">allarmi</TabsContent>
       </Tabs>
     </main>
   );
