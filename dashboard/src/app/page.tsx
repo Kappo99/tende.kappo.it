fb0e62eb660dc8ba11352673d7221a7ba78fb159
Andrea Cappone <andrea30.4.99@gmail.com>
2026-01-27 20:39:18 +0100

    fix Tabs

--- dashboard/src/app/page.tsx
+++ dashboard/src/app/page.tsx
@@ -4,14 +4,16 @@ import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
 export default function Home() {
   return (
     <main className="container mx-auto px-4 py-8">
-      <Tabs defaultValue="account" className="w-100">
-        <div className="flex justify-between w-full">
+      <Tabs defaultValue="vento" className="w-full">
+        <div className="flex justify-between">
           <TabsList>
             <TabsTrigger value="vento">SENSORE VENTO</TabsTrigger>
             <TabsTrigger value="pioggia">SENSORE PIOGGIA</TabsTrigger>
             <TabsTrigger value="allarmi">REGISTRO ALLARMI</TabsTrigger>
           </TabsList>
-          <Input/>
+          <div>
+            <Input />
+          </div>
         </div>
         <TabsContent value="vento">Make changes to your account here.</TabsContent>
         <TabsContent value="pioggia">Change your password here.</TabsContent>
